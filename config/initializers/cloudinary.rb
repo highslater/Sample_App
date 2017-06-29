3a0b37c8f61ea0e51c9122209fef5f1f08a81008
highslater <highslater@hotmail.com>
2017-06-29 16:30:18 -0400

    commit

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'hee6bj1we'
+  config.api_key = '598429816194491'
+  config.api_secret = 'j2V8bRWk4wFlp9zv45fI3KpR_-k'
+  config.cdn_subdomain = true
+end
