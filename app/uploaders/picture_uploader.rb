e35efec37228a1152932e20edda218df45bc525c
highslater <highslater@hotmail.com>
2017-06-29 15:15:51 -0400

    commit

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,19 +6,27 @@ class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   process resize_to_limit: [400, 400]
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
   # storage :fog
-  if Rails.env.production?
-    # storage :fog
-    storage :file
-  else
-    storage :file
-  end
+
+
+  # if Rails.env.production?
+  #   # storage :fog
+  #   storage :file
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+
+    if Rails.env.production?
+      "#{Rails.root}/tmp/uploads"
+    else
+      "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    end
+
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
